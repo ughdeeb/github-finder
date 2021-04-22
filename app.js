eceb11cada0f79cd20c29101d8eda8acefb1002a
ughdeeb <adeebsyed725@gmail.com>
2021-04-21 23:06:25 -0400

    final

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init Github
 const github = new Github();
+// Init UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById("searchUser");
@@ -10,15 +12,19 @@ searchUser.addEventListener("keyup", (e) => {
   const userText = e.target.value;
 
   if (userText !== "") {
-    //Make HTTP CALL
+    // Make http call
     github.getUser(userText).then((data) => {
       if (data.profile.message === "Not Found") {
-        //Show Alert
+        // Show alert
+        ui.showAlert("User not found", "alert alert-danger");
       } else {
-        //Show Profile
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
-    //Clear the profile
+    // Clear profile
+    ui.clearProfile();
   }
 });
